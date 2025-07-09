82a67bb4daa5051802ef95fb4a9b72c3baeaa8e8
Nahom Hul <nahomhulum@gmail.com>
2025-07-09 09:33:07 +0300

    customizing admin

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -6,4 +6,5 @@ admin.site.register(Book)
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
+    list_filter = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
