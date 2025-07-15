b841fa449fdd16cefcf81e3e53afef248b5f3424
Nahom Hul <nahomhulum@gmail.com>
2025-07-15 14:45:39 +0300

    class based views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Book
+from .models import Book, Library
+#Import generic classes for django generic view
+from django.views.generic import DetailView
+
 
 # Create your views here.
 
@@ -10,3 +13,15 @@ def get_books(request):
         "books": books
     }
     return render(request, 'relationship_app/list_books.html', context )
+
+
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'  # Update with your template path
+    context_object_name = 'library'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.object.books.all()  # Fetch all books related to the library
+        return context
