49494d8b12dbd3148b7e4d89f61d60ca44cedd63
Nahom Hul <nahomhulum@gmail.com>
2025-07-15 17:32:03 +0300

    adding class based views in auth

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from .views import list_books
-from .views import LibraryDetailView, RegisterView
+from .views import LibraryDetailView
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
     path('list_books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('register', RegisterView.as_view(), name='register'),
+    path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(template_name= 'relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html', name='logout'),),
 ]
