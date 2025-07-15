688eab0edd58e602a834dff39f6a02c6c771cc65
Nahom Hul <nahomhulum@gmail.com>
2025-07-15 17:12:44 +0300

    defining urls

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import list_books
-from .views import LibraryDetailView
+from .views import LibraryDetailView, SignUpView
+from . import views
 
 urlpatterns = [
     path('list_books/', list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail')
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('register', SignUpView.as_view(), name='register')
 ]
