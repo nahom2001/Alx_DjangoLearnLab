89e62765066333a3963e4a769e2849f06089d545
Nahom Hul <nahomhulum@gmail.com>
2025-07-15 15:23:15 +0300

    adding autehntication login

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
+from django.contrib.auth import login
 
 
 # Create your views here.
