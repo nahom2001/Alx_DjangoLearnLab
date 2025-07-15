a1466d950b7c655de3da4e72e2798c95634b2f77
Nahom Hul <nahomhulum@gmail.com>
2025-07-15 12:35:38 +0300

    first view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book
 
 # Create your views here.
+
+def get_books(request):
+    books = Book.objects.all()
+    return books
