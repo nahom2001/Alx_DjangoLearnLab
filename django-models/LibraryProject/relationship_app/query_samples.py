4e5c7bfed521ff307a7ce9c2cf617a96c57dc025
Nahom Hul <nahomhulum@gmail.com>
2025-07-15 12:06:53 +0300

    books.all()

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -6,8 +6,9 @@ def get_books_by_author(specific_author):
 
 def get_books_in_library(library_name):
     library = Library.objects.get(name=library_name)
-    books_in_library = Library.objects.all()
-    return books_in_library
+
+    books = Library.objects.all()
+    return books.all()
 
 def get_librarian(specific_library):
     librarian = Library.objects.get(library__name=specific_library)    
