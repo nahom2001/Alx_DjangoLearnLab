f02115751565fed6683ac1bf7f58ecaae13f8253
Nahom Hul <nahomhulum@gmail.com>
2025-07-15 15:03:40 +0300

    adding class based views in urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import LibraryDetailView
 
 urlpatterns = [
     path('get_books/', views.get_books, name='get_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail')
 ]
