9f3b574c73aa5696b4235062bc004deb14595f96
Nahom Hul <nahomhulum@gmail.com>
2025-07-16 10:05:32 +0300

    Role based auth

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
@@ -29,5 +28,15 @@ class Librarian(models.Model):
     def __str__(self):
         return self.name
 
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ('admin', 'Admin'),
+        ('librarian', 'Librarian'),
+        ('member', 'Member'),
+    ]
 
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
 
+    def __str__(self):
+        return f"{self.user.username} - {self.get_role_display()}"
