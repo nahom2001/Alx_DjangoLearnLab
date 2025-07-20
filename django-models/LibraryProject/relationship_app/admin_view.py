a3b3f820e70ee087c4379be9f7f4419117b959cf
Nahom Hul <nahomhulum@gmail.com>
2025-07-20 11:19:02 +0300

    Role based auth

--- django-models/LibraryProject/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -3,16 +3,10 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test, login_required
 
 def is_admin(user):
-    if not user.is_authenticated:
-        return False
-    try:
-        return user.profile.role == 'Admin'
-    except AttributeError:
-        return False
-    return user.is_authenticated and hasattr(user, 'profile') and user.profile.role == 'Admin'
+    print("DEBUG Role:", getattr(user, "profile", None) and user.profile.role)
+    return user.is_authenticated and hasattr(user, "profile") and user.profile.role == 'Admin'
 
-
-@user_passes_test(is_admin, login_url='/login/')
+@user_passes_test(is_admin)
 def admin_view(request):
+    print("DEBUG: Admin view accessed by", request.user)
     return render(request, 'admin_view.html')
-
