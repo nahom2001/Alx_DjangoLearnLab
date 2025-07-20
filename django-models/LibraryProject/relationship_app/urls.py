5f887b41f053b180b13260b9295a3d1c1401aff5
Nahom Hul <nahomhulum@gmail.com>
2025-07-20 11:14:59 +0300

    Role based auth

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(template_name= 'relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout',),
-    path('admin-view/', admin_view, name='admin_view'),
+    path('admin-view/', .views.admin_view, name='admin_view'),
     path('librarian-view/', librarian_view, name='librarian_view'),
     path('member-view/', member_view, name='member_view'),
 ]
