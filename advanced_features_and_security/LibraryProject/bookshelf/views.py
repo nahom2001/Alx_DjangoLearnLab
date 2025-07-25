0413e9b29ffc6dfc1d7c4046579a9f65c6e3045f
Nahom Hul <nahomhulum@gmail.com>
2025-07-25 15:05:54 +0300

    adding permission decorators and exceptions

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.contrib.auth.decorators import login_required, permission_required
 from .models import Book  # Assuming you have a Book model
 
+@login_required
+@permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
     # Fetch all books from the database
     books = Book.objects.all()
