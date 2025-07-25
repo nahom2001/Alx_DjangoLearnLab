cbec55774be90438e5d638a04238ac2a0249a305
Nahom Hul <nahomhulum@gmail.com>
2025-07-25 14:24:06 +0300

    remove files

--- advanced_features_and_security/LibraryProject/LibraryProject/urls.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/urls.py
@@ -1,15 +1,22 @@
-# LibraryProject/LibraryProject/urls.py
+"""
+URL configuration for LibraryProject project.
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView # Keep this if you have a home.html
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('bookshelf/', include('bookshelf.urls')),
-    path('relationships/', include('practice_relationships.urls')),
-    path('relationships_app/', include('relationship_app.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'), # Corrected line   
 ]
--- advanced_features_and_security/LibraryProject/practice_relationships/static/practice_relationships/js/script.js
+++
@@ -1,4 +0,0 @@
-// File: LibraryProject/practice_relationships/static/practice_relationships/js/script.js
-
-console.log("Script loaded successfully!");
-// alert("Hello from JavaScript!"); // You can uncomment this to see an immediate alert
