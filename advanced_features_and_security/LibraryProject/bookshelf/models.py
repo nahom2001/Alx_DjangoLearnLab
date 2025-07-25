b365b015d634c258ac78e32552a41eaf551aec1d
Nahom Hul <nahomhulum@gmail.com>
2025-07-25 14:53:01 +0300

    specifying permissions

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -12,6 +12,16 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.title} by {self.author} ({self.publication_year})"
+    
+    class Meta:
+        permissions = [
+            ("can_view", "Can View Book"),
+            ("can_create", "Can Create Book"),
+            ("can_edit", "Can Edit Book"),
+            ("can_delete", "Can Delete Book"),
+        ]
+
+#permissions
 
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(
@@ -35,3 +45,4 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault('date_of_birth', None)  # Optional for superuser
         extra_fields.setdefault('profile_photo', None)  # Optional for superuser
         return super().create_superuser(username, email, password, **extra_fields)
+
