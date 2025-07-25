90f848f19b70e6568e75a95e343eff9ce8249c31
Nahom Hul <nahomhulum@gmail.com>
2025-07-25 15:04:32 +0300

    setting up basic view

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Book  # Assuming you have a Book model
+
+def book_list(request):
+    # Fetch all books from the database
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
