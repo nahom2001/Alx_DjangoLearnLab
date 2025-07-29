de56c1ebd7071cfa0dd6b6bc68b7d475d637d7b4
Nahom Hul <nahomhulum@gmail.com>
2025-07-29 14:35:08 +0300

    first api view

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import BookSerializer
+from .models import Book
 
 # Create your views here.
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        return queryset
+
+#In api/views.py, create a view named BookList that extends rest_framework.generics.ListAPIView.
+#Use the BookSerializer to serialize the data and the Book model as the queryset.
