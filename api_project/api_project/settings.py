5650460f14ce44997c036b2839c2433b5be37177
Nahom Hul <nahomhulum@gmail.com>
2025-07-29 15:29:15 +0300

    authentication setup

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -41,6 +41,16 @@ INSTALLED_APPS = [
     'api', #django app
 ]
 
+
+#adding authentication classes
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
