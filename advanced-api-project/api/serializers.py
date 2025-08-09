b877bfe0f2c47436e71033d5398b36db2532a118
Nahom Hul <nahomhulum@gmail.com>
2025-08-09 10:53:33 +0300

    adding explanatory comments

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 from .models import Author, Book
-import datetime
+import datetime # imported module to get access to the current year
 
+# Used to serialize the 'Book' model
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fieds = '__all__'
 
+    # function used to make sure that the publication year is not in the future. value is obtained from the request object
     def validate(self, value):
         current_year = datetime.date.today().year
         if value > current_year:
