15bb693c33f9e1c0dae562449c5cafbb509c4fae
Nahom Hul <nahomhulum@gmail.com>
2025-08-11 09:44:01 +0300

    installing the blog app

--- django_blog/django_blog/django_blog/settings.py
+++ django_blog/django_blog/django_blog/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'blog',
+    'blog', # installing the app
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
