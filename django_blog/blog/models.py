a8744c23d874bc127c821ac625435dd0168a7819
Nahom Hul <nahomhulum@gmail.com>
2025-08-11 09:54:19 +0300

    defining the models

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
