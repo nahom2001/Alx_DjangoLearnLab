17359f292c5982344d86760d80efee8d5b6d9347
Nahom Hul <nahomhulum@gmail.com>
2025-08-11 15:48:22 +0300

    adding endpoints

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from django.contrib.auth import views as  auth_views
 
 urlpatterns = [
-    path('login', LoginView.as_view(), name='login'),
-    path('logout', LogoutView.as_view(), name='logout'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register')
 ]
+
+# blog/urls.py doesn't contain: ["login/", "register/", "profile/"]
