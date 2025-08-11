9775650bd8d98bc1c67ee332a15ee16c4f619b2a
Nahom Hul <nahomhulum@gmail.com>
2025-08-11 16:22:10 +0300

    ensure that the user pases the test

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,80 +1,79 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post  # Assuming your blog post model is named Post
 from .forms import CustomUserCreationForm, UserProfileForm
 
-class ListView(ListView):
+# ListView to display all blog posts
+class BlogListView(ListView):
     model = Post
-    template_name = 'blog/post_list.html'  # Template to    of 
-    context_object_name = 'posts'  
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
 
-class DetailView(DetailView):
+# DetailView to show individual blog posts
+class BlogDetailView(DetailView):
     model = Post
-    template_name = 'blog/post_detail.html'  
-    context_object_name = 'post'  
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
 
-@method_decorator(login_required, name='dispatch')
-class CreateView(CreateView):
+# CreateView to allow authenticated users to create new posts
+class BlogCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    template_name = 'blog/post_form.html'  
-    fields = ['title', 'content']  
-    success_url = reverse_lazy('post_list')  
+    template_name = 'blog/post_create.html'
+    fields = ['title', 'content']
+    success_url = reverse_lazy('post_list')
 
-@method_decorator(login_required, name='dispatch')
-class UpdateView(UpdateView):
+# UpdateView to enable post authors to edit their posts
+class BlogUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
-    template_name = 'blog/post_form.html'  
-    fields = ['title', 'content']  # Fields to edit
-    success_url = reverse_lazy('post_list')  # Redirect after successful update
+    template_name = 'blog/post_update.html'
+    fields = ['title', 'content']
+    success_url = reverse_lazy('post_list')
 
-    def get_queryset(self):
-        # Ensure only the author can edit their post
-        return Post.objects.filter(author=self.request.user)
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author  # Ensure only the author can edit
 
 # DeleteView to let authors delete their posts
-@method_decorator(login_required, name='dispatch')
-class DeleteView(DeleteView):
+class BlogDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
-    template_name = 'blog/post_confirm_delete.html'  # Template for confirming deletion
-    success_url = reverse_lazy('post_list')  # Redirect after successful deletion
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('post_list')
 
-    def get_queryset(self):
-        # Ensure only the author can delete their post
-        return Post.objects.filter(author=self.request.user)
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author  # Ensure only the author can delete
 
 # Registration and Authentication Views
 class RegisterView(CreateView):
     form_class = CustomUserCreationForm
     template_name = 'registration/register.html'
-    success_url = reverse_lazy('login')  
+    success_url = reverse_lazy('login')
 
 class CustomLoginView(LoginView):
     template_name = 'registration/login.html'
-    success_url = reverse_lazy('profile')  
+    success_url = reverse_lazy('profile')
 
 class CustomLogoutView(LogoutView):
-    template_name = 'registration/logout.html'  
+    template_name = 'registration/logout.html'
 
 # Profile Management View
-@method_decorator(login_required, name='dispatch')
-class ProfileView(UpdateView):
+class ProfileView(LoginRequiredMixin, UpdateView):
     form_class = UserProfileForm
     template_name = 'profile.html'
     success_url = reverse_lazy('profile')
 
     def get_object(self, queryset=None):
-        return self.request.user  # Return the current user
+        return self.request.user
 
     def dispatch(self, request, *args, **kwargs):
         if request.method == 'POST':
-            return self.post(request, *args, **kwargs)  # Handle POST requests
-        return super().dispatch(request, *args, **kwargs)  # Handle GET requests
+            return self.post(request, *args, **kwargs)
+        return super().dispatch(request, *args, **kwargs)
 
     def form_valid(self, form):
-        form.save()  # Save the updated user information
-        return super().form_valid(form)  # Redirect to success_url
+        form.save()
+        return super().form_valid(form)
 
     def form_invalid(self, form):
         return self.render_to_response(self.get_context_data(form=form))
