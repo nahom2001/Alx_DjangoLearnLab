655fa0191ece2ca8841f4653edcd7ae8c8f4ec38
Nahom Hul <nahomhulum@gmail.com>
2025-08-11 16:11:48 +0300

    adding routes for CRUD operations

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from django.contrib.auth import views as  auth_views
+from .views import ListView, CreateView, UpdateView, DetailView, DeleteView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/', RegisterView.as_view(), name='register')
-]
+    path('register/', RegisterView.as_view(), name='register'),
+    path('post/', ListView.as_view(), name='posts'),
+    path('post/new', CreateView.as_view(), name='new-post'),
+    path('post/<int:pk>/detail', DetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update', UpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete', DeleteView.as_view(), naem='post-delete'),
 
-# blog/urls.py doesn't contain: ["login/", "register/", "profile/"]
+]
