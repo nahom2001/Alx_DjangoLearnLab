2207b8545992e970e0b0cd5f2d8e13d7f470e431
Nahom Hul <nahomhulum@gmail.com>
2025-08-11 15:54:06 +0300

    implement views

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,29 @@
+from django.views.generic import CreateView, UpdateView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
 from django.shortcuts import render
+from .forms import CustomUserCreationForm, UserProfileForm
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
-# Create your views here.
+class RegisterView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy('login')  
+
+class CustomLoginView(LoginView):
+    template_name = 'registration/login.html'
+    success_url = reverse_lazy('profile')  
+
+class CustomLogoutView(LogoutView):
+    template_name = 'registration/logout.html'  
+
+# Profile Management View
+@method_decorator(login_required, name='dispatch')
+class ProfileView(UpdateView):
+    form_class = UserProfileForm
+    template_name = 'profile.html'
+    success_url = reverse_lazy('profile')
+
+    def get_object(self, queryset=None):
+        return self.request.user  # Return the current user
