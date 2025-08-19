2bdef404d6ae9c5c883c639c097150fbd9cb365b
Nahom Hul <nahomhulum@gmail.com>
2025-08-19 10:31:40 +0300

    first view draft

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
-
 # Create your views here.
+from rest_framework import viewsets, permissions
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from django.contrib.auth import get_user_model
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+
+User = get_user_model()
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def perform_create(self, request):
+        # Set the author to the current user during creation
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(author=self.request.user)
+        return serializer
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def perform_create(self, request):
+        # Set the author to the current user during creation
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(author=self.request.user)
+        return serializer
