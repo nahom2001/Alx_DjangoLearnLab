6970d2ebc3e43aaacd3d03eb792fd833fcbd8bac
Nahom Hul <nahomhulum@gmail.com>
2025-08-19 10:05:30 +0300

    write serializers

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'bio', 'profile_picture']  #serializes all the fields
+
+class TokenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Token
+        fields = ['key'] 
