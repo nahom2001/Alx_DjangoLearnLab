dc2d82e662b92872a91b90cd7a7559b3c2139379
Nahom Hul <nahomhulum@gmail.com>
2025-08-19 10:38:11 +0300

    following attribute

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -7,7 +7,10 @@ class CustomUser(AbstractUser):
     bio = models.TextField(blank=True)
     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
     followers = models.ManyToManyField('self', symmetrical=False, related_name='followed_by', blank=True)
+    following = models.ManyToManyField('self', symmetrical=False, related_name='followers', blank=True)
 
+    def __str__(self):
+        return self.username
 
 
 
