084bcec9d6b0d3e62d627a94bbc3822404d3761e
Nahom Hul <nahomhulum@gmail.com>
2025-08-19 11:01:59 +0300

    update post serializers

--- social_media_api/posts/serializers.py
+++ social_media_api/posts/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from .models import Post, Comment
+from .models import Post, Comment, Like
 
 User = get_user_model()
 
@@ -14,11 +14,15 @@ class PostSerializer(serializers.ModelSerializer):
     author_id = serializers.PrimaryKeyRelatedField(
         queryset=User.objects.all(), source='author', write_only=True
     )
+    like_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
-        fields = ['id', 'author', 'author_id', 'title', 'content', 'created_at', 'updated_at']
-        read_only_fields = ['created_at', 'updated_at']
+        fields = ['id', 'author', 'author_id', 'title', 'content', 'created_at', 'updated_at', 'like_count']
+        read_only_fields = ['created_at', 'updated_at', 'like_count']
+
+    def get_like_count(self, obj):
+        return obj.likes.count()
 
     def validate_title(self, value):
         if not value.strip():
