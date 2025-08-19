960f64728310f3b1a3ab13d7196293df84e29837
Nahom Hul <nahomhulum@gmail.com>
2025-08-19 11:15:52 +0300

    Securing app via https

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -88,11 +88,14 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',  # Use SQLite
+        'NAME': BASE_DIR / 'db.sqlite3',  
+        'USER': '',  
+        'PASSWORD': '',  
+        'HOST': '',  
+        'PORT': '',  
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
