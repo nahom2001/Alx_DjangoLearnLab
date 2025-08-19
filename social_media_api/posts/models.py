df8da9a7d0ce039440b8fd64ce9affe29d68029b
Nahom Hul <nahomhulum@gmail.com>
2025-08-19 10:56:37 +0300

    error

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -4,6 +4,12 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+# Create your models here.
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     title = models.CharField(max_length=200)
@@ -25,3 +31,12 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.author.username} on {self.post.title}"
+
+
+class Like(models.Model):
+    pass
+
+
+
+# In the posts app, create a Like model that tracks which users have liked which posts. This model should have a ForeignKey to Post and a ForeignKey to User.
+# In a new app called notifications, create a Notification model with fields like recipient (ForeignKey to User), actor (ForeignKey to User), verb (describing the action), target (GenericForeignKey to the object), and timestamp.
