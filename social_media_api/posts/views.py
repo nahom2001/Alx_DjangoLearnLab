7ca9f99fd5919e1c801f20e49bacfdd4b5b54ffb
Nahom Hul <nahomhulum@gmail.com>
2025-08-19 10:45:44 +0300

    update

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
-# Create your views here.
 from rest_framework import viewsets, permissions
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
@@ -10,8 +9,10 @@ User = get_user_model()
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
+        # Allow read-only access (GET, HEAD, OPTIONS) for all users
         if request.method in permissions.SAFE_METHODS:
             return True
+        # Allow write access (POST, PUT, DELETE) only if the user is the author
         return obj.author == request.user
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -37,3 +38,13 @@ class CommentViewSet(viewsets.ModelViewSet):
         serializer.is_valid(raise_exception=True)
         serializer.save(author=self.request.user)
         return serializer
+
+class FeedView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        # Get posts from users the current user follows
+        following_users = request.user.following.all()
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
