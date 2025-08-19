9cbffaea5210f5189ace39aa59f3872d3491692c
Nahom Hul <nahomhulum@gmail.com>
2025-08-19 10:11:39 +0300

    serializers v2

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,7 +5,7 @@ from rest_framework.authtoken.models import Token
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField()
 
     class Meta:
         model = User
@@ -13,14 +13,14 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
+            password=validated_data['password'],
             bio=validated_data.get('bio', ''),
             profile_picture=validated_data.get('profile_picture', None)
         )
-        user.set_password(validated_data['password'])  # Hash the password
-        user.save()
+        Token.objects.create(user=user)
         return user
 
 class TokenSerializer(serializers.ModelSerializer):
