1b4b802a479fcabf4aaee74a1777aa9a62eb438e
Nahom Hul <nahomhulum@gmail.com>
2025-08-19 10:07:11 +0300

    update serializer to fit

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,11 +5,25 @@ from rest_framework.authtoken.models import Token
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture']  #serializes all the fields
+        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None)
+        )
+        user.set_password(validated_data['password'])  # Hash the password
+        user.save()
+        return user
 
 class TokenSerializer(serializers.ModelSerializer):
     class Meta:
         model = Token
-        fields = ['key'] 
+        fields = ['key']
